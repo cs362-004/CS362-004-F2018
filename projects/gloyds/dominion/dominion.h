4c5a19b2ace8bfc90e637a036937359b6360ed13
Steven Gloyd <jemimacakes@gmail.com>
2018-10-07 20:18:48 -0700

    Refactors adventurer card functionality into function playAdventurer()

--- projects/gloyds/dominion/dominion.h
+++ projects/gloyds/dominion/dominion.h
@@ -128,4 +128,8 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
+int playAdventurer(struct gameState* state, int currentPlayer, int* temphand, int* z);
+/* Play the adventurer card and edit the 
+   game state as necessary */
+
 #endif
